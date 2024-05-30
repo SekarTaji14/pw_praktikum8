0c03445b8c3533b1b8d07ae882697a1d96ee49ff
sekartajii <sekartaji@students.amikom.ac.id>
2024-05-30 22:43:20 +0700

    update

---
+++ src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div>
+        <h1>Ini adalah komponen artikel home</h1>
+        <p>Ini adalah komponen artikel home</p>
+        <p>Ini adalah komponen artikel home</p>
+        <p>Ini adalah komponen artikel home</p>
+        <p>Ini adalah komponen artikel home</p>
+    </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import ArtikelHome from '../components/ArtikelHome.vue'
+</script>
+
+
+<template>
+  <main>
+  <img alt="img" class="img" src="@/assets/hiu.jpg" width="125" height="125" />
+  <h1>Custom Halaman</h1>
+  <ArtikelHome/>
+    <!--<TheWelcome />-->
+  </main>
+</template>
